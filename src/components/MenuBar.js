50cdc0473baa240a743c98c22912ea48358ac1cc
jkhaha <jha2@wellesley.edu>
2018-11-01 10:48:05 -0400

    done

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,37 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-const MenuBar = (props) => {
-
+class MenuBar extends Component {
+render() {
   /*
-
-  The 'a' tags below are the menu items. Think about the way a menu 
+  The 'a' tags below are the menu items. Think about the way a menu
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
   this programatically? What other behavior do we expect when we click
   on a menu item? Do we need state in this component, and if not, how can
   this component be made aware of what is currently the active menu item?
-
   */
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a onClick={this.props.handleProfileActive}className="item active" id="profile">
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a onClick={this.props.handlePhotoActive} className="item" id="photo">
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a onClick={this.props.handleCocktailActive}className="item" id="cocktail">
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a onClick={this.props.handlePokemonActive} className="item" id="pokemon">
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
   )
+  }
 
 }
 
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -4,21 +4,81 @@ import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
 
+  state = {
+    profileActive: true,
+    photoActive: false,
+    cocktailActive: false,
+    pokemonActive: false
+  }
+
+  handleProfileActive = () => {
+    if (this.state.profileActive) {
+      this.setState({profileActive: false})
+    }
+    else {
+      this.setState({profileActive: true})
+      this.setState({photoActive: false})
+      this.setState({cocktailActive: false})
+      this.setState({pokemonActive: false})
+    }
+  }
+
+  handlePhotoActive = () => {
+    if (this.state.photoActive) {
+      this.setState({photoActive: false})
+    }
+    else {
+      this.setState({photoActive: true})
+      this.setState({profileActive: false})
+      this.setState({cocktailActive: false})
+      this.setState({pokemonActive: false})
+    }
+  }
+
+  handleCocktailActive = () => {
+    if (this.state.cocktailActive) {
+      this.setState({cocktailActive: false})
+    }
+    else {
+      this.setState({cocktailActive: true})
+      this.setState({pokemonActive: false})
+      this.setState({photoActive: false})
+      this.setState({profileActive: false})
+    }
+  }
+
+  handlePokemonActive = () => {
+    if (this.state.pokemonActive) {
+      this.setState({pokemonActive: false})
+    }
+    else {
+      this.setState({pokemonActive: true})
+      this.setState({cocktailActive: false})
+      this.setState({photoActive: false})
+      this.setState({profileActive: false})
+    }
+  }
+
+
+
 
   render() {
-
     /*
-
-    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
-
+    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.
+    Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state?
+    Where should these methods be called?
     */
-
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
-
     return (
       <div>
-        <MenuBar />
-        {detailsToDisplay}
+        <MenuBar
+          handleProfileActive = {this.handleProfileActive}
+          handlePhotoActive = {this.handlePhotoActive}
+          handleCocktailActive = {this.handleCocktailActive}
+          handlePokemonActive = {this.handlePokemonActive}/>
+        <div>{this.state.photoActive === true ? <Photos /> : " "}</div>
+        <div>{this.state.profileActive === true ? <Profile /> : " "}</div>
+        <div>{this.state.cocktailActive === true ? <Cocktails /> : " "}</div>
+        <div>{this.state.pokemonActive === true ? <Pokemon/> : " "}</div>
       </div>
     )
   }
